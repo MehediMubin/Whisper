28db5414f05738fa5b52448a9d94879e96c8ad0c
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-10-25 15:59:54 +0600

    feat: add protectRoute middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import UserModel from "../models/userModel";
+
+export const protectRoute = async (req, res, next) => {
+   try {
+      const token = req.headers.token;
+      const { userId } = jwt.verify(token, process.env.JWT_SECRET);
+
+      const user = UserModel.findById(userId).select("-password");
+      if (!user) {
+         res.json({
+            success: false,
+            message: "User not found",
+         });
+      }
+
+      req.user = user;
+      next();
+   } catch (error) {
+      console.error(error.message);
+      res.json({
+         success: false,
+         message: error.message,
+      });
+   }
+};
