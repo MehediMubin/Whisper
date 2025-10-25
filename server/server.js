a9a538d6deca1f4229d1d91ba84ca09545dd6b60
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-10-25 18:41:51 +0600

    feat: add socket.io connection

--- server/server.js
+++ server/server.js
@@ -10,6 +10,35 @@ import userRouter from "./routes/userRoutes.js";
 const app = express();
 const server = http.createServer(app);
 
+// Initialize socket.io server
+const io = new Server(server, {
+   cors: {
+      origin: "*",
+   },
+});
+
+// Store online users
+export const userSocketMap = {}; // { userId: socketId }
+
+// Socket.io connection handling
+io.on("connection", (socket) => {
+   const userId = socket.handshake.query.userId;
+   console.log("User Connected", userId);
+
+   if (userId) {
+      userSocketMap[userId] = socket.id;
+   }
+
+   // Emit online users to all connected clients
+   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+   socket.on("disconnect", () => {
+      console.log("User Disconnected", userId);
+      delete userSocketMap[userId];
+      io.emit("getOnlineUsers", Object.keys(userSocketMap));
+   });
+});
+
 // Middleware setup
 app.use(cors());
 app.use(express.json({ limit: "4mb" }));
