49c9b26ef116304f3115acdf41f84e3ca8a34400
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-10-25 18:46:02 +0600

    feat: add send message route

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,9 +1,16 @@
-import express from 'express';
-import { getUsersForSidebar, getMessages, markMessageAsSeen } from '../controllers/messageController';
-import { protectRoute } from '../middleware/auth';
+import express from "express";
+import {
+   getMessages,
+   getUsersForSidebar,
+   sendMessage,
+} from "../controllers/messageController";
+import { protectRoute } from "../middleware/auth";
 
 const messageRouter = express.Router();
 
-messageRouter.get('/users', protectRoute, getUsersForSidebar);
-messageRouter.get('/:id', protectRoute, getMessages);
-messageRouter.put('/mark/:id', protectRoute, markMessagesAsSeen);
+messageRouter.get("/users", protectRoute, getUsersForSidebar);
+messageRouter.get("/:id", protectRoute, getMessages);
+messageRouter.put("/mark/:id", protectRoute, markMessagesAsSeen);
+messageRouter.post("/send/:id", protectRoute, sendMessage);
+
+export default messageRouter;
