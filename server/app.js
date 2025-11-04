e934387645f9aa532dd584df927084af715cb810
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-11-04 18:32:57 +0600

    chore: add morgan request logging to Express app

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import cors from "cors";
 import express from "express";
+import morgan from "morgan";
 import messageRouter from "./src/routes/messageRoutes.js";
 import userRouter from "./src/routes/userRoutes.js";
 
@@ -7,6 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json({ limit: "4mb" }));
 
 // Routes
